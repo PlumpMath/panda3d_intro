f846b4c65b443ad8ac9fce46143b491ded7e97ec
Maria <parody@uw.edu>
2015-07-07 15:28:15 -0700

    fixed bug in camera spin restarting

--- IntroApp.py
+++ IntroApp.py
@@ -2,7 +2,7 @@ from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
 from math import pi, sin, cos
 import sys
- 
+
 
 class MyApp(ShowBase):
  
@@ -24,28 +24,35 @@ class MyApp(ShowBase):
         base.disableMouse()
         # set up some keyboard input
         self.accept("escape", sys.exit)  # Escape quits
-        self.accept("space", self.toggle_camera_spin)  # spacebar 
-
+        self.accept("space", self.toggle_camera_spin)  # spacebar
+        
         # set up a variable to keep track of camera toggle
         self.spin_camera = True
+        # variable to keep track of total time spinning
+        self.spin_time = 0
         # go ahead and start camera spinning
         self.toggle_camera_spin()
-        
+
     def toggle_camera_spin(self):
-		if self.spin_camera:
-			self.taskMgr.add(self.spin_camera_task, "SpinCameraTask")
-		else:
+        if self.spin_camera:
+            self.spin_task = self.taskMgr.add(self.spin_camera_task, "SpinCameraTask")
+            # set up task variable to use to determine dt
+            self.spin_task.last = 0
+        else:
 			self.taskMgr.remove("SpinCameraTask")
-		self.spin_camera = not self.spin_camera
+        self.spin_camera = not self.spin_camera
         
     def spin_camera_task(self, task):
         # Define a method to move the camera.
-		angle_degrees = task.time * 6.0
-		angle_radians = angle_degrees * (pi / 180.0)
-		self.camera.setPos(20 * sin(angle_radians), -20.0 * cos(angle_radians), 3)
-		self.camera.setHpr(angle_degrees, 0, 0)
-		return task.cont
+        dt = task.time - task.last
+        task.last = task.time
+        self.spin_time += dt
+        angle_degrees = self.spin_time * 6.0
+        angle_radians = angle_degrees * (pi / 180.0)
+        self.camera.setPos(20 * sin(angle_radians), -20.0 * cos(angle_radians), 3)
+        self.camera.setHpr(angle_degrees, 0, 0)
+        return task.cont
 
 if __name__ == "__main__":
-	app = MyApp()
-	app.run()
+    app = MyApp()
+    app.run()
